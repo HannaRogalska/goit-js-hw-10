d88e1339fd078a651bd479d5183c6677be3d9d54
Hanna Rogalska <hanrogalska@gmail.com>
2023-03-17 16:51:19 +0100

    added file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
+export const fetchCountries = name => {
+  const BASE_URL = 'https://restcountries.com/v3.1/name/';
+  const properties = 'fields=name,capital,population,flags,languages';
+
+  return fetch(`${BASE_URL}${name}?${properties}`).then(response => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+
