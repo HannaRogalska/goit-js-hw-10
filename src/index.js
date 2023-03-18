491e0756264479ab5e728a0c02f53a7eaff13122
Hanna Rogalska <hanrogalska@gmail.com>
2023-03-18 12:59:59 +0100

    added js file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import './css/styles.css';
 const DEBOUNCE_DELAY = 300;
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+
 
 
